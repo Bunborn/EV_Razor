398769d96d5948fff03bf0eb63bde18fe813ef34
Brandon Stevens <bdstevens@crimson.ua.edu>
2021-12-01 14:28:36 -0600

    adds startup function to load vehicle parameter variables

---
+++ loadVariables.m
@@ -0,0 +1,6 @@
+igi0 = 7.0518; % Overall gear ratio
+Cr = 0.011; % Rolling resistance coefficient
+Cd = 0.23; % Aerodynamic drag coefficient
+rd = 0.325; % wheel radius (rim and tire), m
+m = 1757.67; % mass (kg)
+Av = 2.37; % frontal area, m^2
