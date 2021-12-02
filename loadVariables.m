26eedc90df06639298a55a05bf1bf82155cec84e
Brandon Stevens <bdstevens@crimson.ua.edu>
2021-12-01 20:01:53 -0600

    Adds motor data and basic motor torque LUT vs speed

--- loadVariables.m
+++ loadVariables.m
@@ -9,3 +9,25 @@ Av = 2.37; % frontal area, m^2
 rho    = 1.20; % kg/m^3
 g      = 9.81; % m/s^2
 alpha  = 0; % grade %
+%% HV Parameters
+
+%% Motor Parameters
+% Based on the Parker GVM210-300R
+Max_Mot_Trq = 527; % Nm
+Mot_Base_Spd = 415.11; % Rad/sec
+Mot_Peak_Power = 246000; % Watts
+%% Generate Motor LUT
+% 8000 RPM is the max motor speed, at 837.75 rad/sec
+MotSpdBrkPts = linspace(0, 837, 100);
+MotTrqPts = [];
+for i = MotSpdBrkPts
+    if i > Mot_Base_Spd % Power Limited
+        TorqueVal = Mot_Peak_Power / i;
+        if TorqueVal > Max_Mot_Trq % Saturate if above max trq val
+            TorqueVal = Max_Mot_Trq;
+        end
+        MotTrqPts = [MotTrqPts, TorqueVal];
+    else % Torque limited
+        MotTrqPts = [MotTrqPts, Max_Mot_Trq];
+    end
+end
