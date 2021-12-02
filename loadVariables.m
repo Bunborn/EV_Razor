53a74a51aa1a52ba1eeacc99b941119e029ff4a8
Brandon Stevens <bdstevens@crimson.ua.edu>
2021-12-01 18:56:30 -0600

    Adds the base Simulink model, more params in startup script

--- loadVariables.m
+++ loadVariables.m
@@ -1,6 +1,11 @@
+%% Vehicle Parameters
 igi0 = 7.0518; % Overall gear ratio
 Cr = 0.011; % Rolling resistance coefficient
 Cd = 0.23; % Aerodynamic drag coefficient
 rd = 0.325; % wheel radius (rim and tire), m
-m = 1757.67; % mass (kg)
+mass = 1757.67; % mass (kg)
 Av = 2.37; % frontal area, m^2
+%% Environment Parameters
+rho    = 1.20; % kg/m^3
+g      = 9.81; % m/s^2
+alpha  = 0; % grade %
